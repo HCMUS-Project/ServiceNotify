a583ddddcf7638e47a4c7391dae3cc526003dd49
Nguyễn Tiến Lợi <ntloi120202@gmail.com>
2024-06-17 13:06:00 +0700

    feat: create notify for order and booking

--- src/feature/events/event.gateway.ts
+++ src/feature/events/event.gateway.ts
@@ -1,3 +1,4 @@
+import { Inject, Injectable } from '@nestjs/common';
 import {
     MessageBody,
     SubscribeMessage,
@@ -5,16 +6,25 @@ import {
     WebSocketServer,
     ConnectedSocket,
 } from '@nestjs/websockets';
-import { from, map } from 'rxjs';
 import { Server, Socket } from 'socket.io';
+import Logger, { LoggerKey } from 'src/core/logger/interfaces/logger.interface';
 
+@Injectable()
 @WebSocketGateway()
 export class EventsGateway {
+
     @WebSocketServer()
     server: Server;
 
+    constructor(
+        @Inject(LoggerKey) private logger: Logger,
+    ) {}
+
+
     @SubscribeMessage('events')
     handleEvent(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+        // console.log('client', client);
+        // return { event: 'events', data };
         this.server.emit('events', data);
         // return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
     }
@@ -22,5 +32,17 @@ export class EventsGateway {
     @SubscribeMessage('identity')
     async identity(@MessageBody() data: number): Promise<number> {
         return data;
+    }
+
+    @SubscribeMessage('booking')
+    async handBooking(@MessageBody() data: string) {
+        this.logger.info('Booking notify with data: ', { props: { data } });
+        this.server.emit('booking-notify', data);
+    }
+
+    @SubscribeMessage('order')
+    async handleOrder(@MessageBody() data: string) {
+        this.logger.info('Order notify with data', { props: { data } });
+        this.server.emit('order-notify', data);
     }
 }
